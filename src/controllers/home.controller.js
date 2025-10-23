aa7bfa993e13e15971c4e8f83cd0083101a9c7bd
Abel Erasmo <erasmosibinde@gmail.com>
2025-10-23 13:01:18 +0200

    Ajustes

---
+++ src/controllers/home.controller.js
@@ -0,0 +1,5 @@
+const getInitialPage = (req, res) => {
+    res.render('index');
+};
+
+export default getInitialPage;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,26 @@
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
+import { fileURLToPath } from "url";
+import cors from "cors";
 import connection from "./config/db_connection.js";
 import { clerkMiddleware } from "./middleware/auth.js";
+import path from "path";
 
 const app = express();
 const PORT = process.env.PORT;
 
-app.use(clerkMiddleware());
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
-connection().then(() => {
-    app.listen(PORT, () => {
-    console.log(`Servidor rodando em http//localhost:${PORT}`);
-});
+app.use(clerkMiddleware());
+    connection().then(() => {
+        app.listen(PORT, () => {
+        console.log(`Servidor rodando em http://localhost:${PORT}`);
+    });
 }) 
 
 
