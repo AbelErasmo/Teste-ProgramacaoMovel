d132ce8cae3cd74a55a002c39c14f40ef2e55e6a
Lenox <168176002+Lenox8@users.noreply.github.com>
2025-10-23 11:37:26 +0200

    Add files via upload

---
+++ middleware/auth.js
@@ -0,0 +1,15 @@
+import { clerkMiddleware } from "@clerk/express";
+
+export const requireAuth = async (req, res, next) => {
+
+    const authorization = await req.auth()  
+
+    if(!authorization || !authorization.userId){
+       return res.status(401).json({Message: "Usuario nao esta cadastrado no sistema. Cadastre-se por favor."})
+    }
+    req.user = authorization.userId
+    next()
+
+}
+
+export default clerkMiddleware  
